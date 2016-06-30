5d783388bbc065fa7528d50a8ea2b15b8db3c934
Fabrizio Chiarello <fabrizio.chiarello@pd.infn.it>
2016-06-30 10:58:53 +0200

    Skeleton of the collector
    Signed-off-by: Fabrizio Chiarello <fabrizio.chiarello@pd.infn.it>

---
+++ src/collector.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+######################################################################
+#
+# Filename: collector.py
+# Created: 2016-06-29T14:32:26+0200
+# Time-stamp: <2016-06-30T10:55:26cest>
+# Author: Fabrizio Chiarello <fabrizio.chiarello@pd.infn.it>
+#
+# Copyright © 2016 by Fabrizio Chiarello
+#
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+######################################################################
+
+import argparse
+import logging
+
+from _version import __version__
+
+
+# LOG SETUP
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
+
+# CLI ARGs
+parser = argparse.ArgumentParser(description='Data collector for CAOS-NG.',
+                                 add_help=True)
+
+parser.add_argument('-v', '--version', action='version',
+                    version='%(prog)s {version}'.format(version=__version__))
+
+parser.add_argument('-c', '--config',
+                    dest='cfg_file', metavar='FILE',
+                    default='collector.conf',
+                    help='configuration file')
+
+
+def main():
+    args = parser.parse_args()
+    cfg_file = args.cfg_file
+    logger.info("Reading configuration file: %s." % cfg_file)
+
+
+if __name__ == "__main__":
+    main()
