e94df2fb361244b86b23e5eaad9077cf2da64557
Fabrizio Chiarello <fabrizio.chiarello@pd.infn.it>
2017-07-25 11:56:02 +0200

    Remove multiple spaces before operators (E221)
    Signed-off-by: Fabrizio Chiarello <fabrizio.chiarello@pd.infn.it>

--- caos_collector/metrics.py
+++ caos_collector/metrics.py
@@ -38,40 +38,40 @@ GAUGE_METRIC = {
     'type': 'gauge'
 }
 
-METRIC_VM_CPU_TIME_USAGE       = 'cpu'
+METRIC_VM_CPU_TIME_USAGE = 'cpu'
 METRIC_VM_WALLCLOCK_TIME_USAGE = 'wallclocktime'
 
-METRIC_VM_VCPUS_USAGE      = 'vm.vcpus.usage'
-METRIC_VM_DISK_USAGE       = 'vm.disk.usage'
-METRIC_VM_MEMORY_USAGE     = 'vm.memory.usage'
-METRIC_VM_COUNT_ACTIVE     = 'vms.active'
-METRIC_VM_COUNT_DELETED    = 'vms.deleted'
+METRIC_VM_VCPUS_USAGE = 'vm.vcpus.usage'
+METRIC_VM_DISK_USAGE = 'vm.disk.usage'
+METRIC_VM_MEMORY_USAGE = 'vm.memory.usage'
+METRIC_VM_COUNT_ACTIVE = 'vms.active'
+METRIC_VM_COUNT_DELETED = 'vms.deleted'
 
-METRIC_QUOTA_MEMORY    = 'quota.memory'
-METRIC_QUOTA_VCPUS     = 'quota.vcpus'
+METRIC_QUOTA_MEMORY = 'quota.memory'
+METRIC_QUOTA_VCPUS = 'quota.vcpus'
 METRIC_QUOTA_INSTANCES = 'quota.instances'
 
 ## hypervisors
-METRIC_HYPERVISOR_STATUS          = 'hypervisor.status'
-METRIC_HYPERVISOR_STATE           = 'hypervisor.state'
+METRIC_HYPERVISOR_STATUS = 'hypervisor.status'
+METRIC_HYPERVISOR_STATE = 'hypervisor.state'
 
-METRIC_HYPERVISOR_CPUS_TOTAL      = 'hypervisor.cpus.total'
-METRIC_HYPERVISOR_VCPUS_TOTAL     = 'hypervisor.vcpus.total'
-METRIC_HYPERVISOR_VCPUS_USED      = 'hypervisor.vcpus.used'
+METRIC_HYPERVISOR_CPUS_TOTAL = 'hypervisor.cpus.total'
+METRIC_HYPERVISOR_VCPUS_TOTAL = 'hypervisor.vcpus.total'
+METRIC_HYPERVISOR_VCPUS_USED = 'hypervisor.vcpus.used'
 
-METRIC_HYPERVISOR_RAM_TOTAL       = 'hypervisor.ram.total'
-METRIC_HYPERVISOR_MEMORY_TOTAL    = 'hypervisor.memory.total'
-METRIC_HYPERVISOR_MEMORY_USED     = 'hypervisor.memory.used'
+METRIC_HYPERVISOR_RAM_TOTAL = 'hypervisor.ram.total'
+METRIC_HYPERVISOR_MEMORY_TOTAL = 'hypervisor.memory.total'
+METRIC_HYPERVISOR_MEMORY_USED = 'hypervisor.memory.used'
 
-METRIC_HYPERVISOR_RUNNING_VMS     = 'hypervisor.vms.running'
-METRIC_HYPERVISOR_WORKLOAD        = 'hypervisor.workload'
-METRIC_HYPERVISOR_LOAD_5m         = 'hypervisor.load.5m'
-METRIC_HYPERVISOR_LOAD_10m        = 'hypervisor.load.10m'
-METRIC_HYPERVISOR_LOAD_15m        = 'hypervisor.load.15m'
+METRIC_HYPERVISOR_RUNNING_VMS = 'hypervisor.vms.running'
+METRIC_HYPERVISOR_WORKLOAD = 'hypervisor.workload'
+METRIC_HYPERVISOR_LOAD_5m = 'hypervisor.load.5m'
+METRIC_HYPERVISOR_LOAD_10m = 'hypervisor.load.10m'
+METRIC_HYPERVISOR_LOAD_15m = 'hypervisor.load.15m'
 
-METRIC_HYPERVISOR_DISK_TOTAL      = 'hypervisor.disk.total'
-METRIC_HYPERVISOR_DISK_USED       = 'hypervisor.disk.used'
-METRIC_HYPERVISOR_DISK_FREE       = 'hypervisor.disk.free'
+METRIC_HYPERVISOR_DISK_TOTAL = 'hypervisor.disk.total'
+METRIC_HYPERVISOR_DISK_USED = 'hypervisor.disk.used'
+METRIC_HYPERVISOR_DISK_FREE = 'hypervisor.disk.free'
 METRIC_HYPERVISOR_DISK_FREE_LEAST = 'hypervisor.disk.free.least'
 
 METRICS = {
